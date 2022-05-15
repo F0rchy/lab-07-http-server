a186a01a1c72c8dd3212bb7afcd1e4ba4a1183f5
F0rchy <theartemka6@mail.ru>
2022-05-15 19:53:45 +0300

    Release 1.0

--- include/example.hpp
+++ include/example.hpp
@@ -1,8 +1,30 @@
-// Copyright 2021 Your Name <your_email>
+// Copyright 2022 Artem Artemov <theartemka6@mail.ru>
 
 #ifndef INCLUDE_EXAMPLE_HPP_
 #define INCLUDE_EXAMPLE_HPP_
 
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/config.hpp>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <shared_mutex>
+#include <stdexcept>
+#include <string>
+#include <thread>
+using json = nlohmann::json;
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
+
 auto example() -> void;
 
-#endif // INCLUDE_EXAMPLE_HPP_
+void go_useful_server(int argc, char* argv[]);
+
+#endif  // INCLUDE_EXAMPLE_HPP_
